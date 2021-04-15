2436ff407f09844da130dc5132d0f3c536f9da10
Peter Christensen <tessercat@pakaa.net>
2021-04-14 19:44:48 -0600

    Add conference config handler.

---
+++ conference/fsapi.py
@@ -0,0 +1,26 @@
+""" Conference app fsapi request handler module. """
+from django.conf import settings
+from fsapi.registries import FsapiHandler, register_fsapi_handlers
+
+
+class ConferenceConfigHandler(FsapiHandler):
+    """ Conference module config request handler. """
+
+    def __init__(self):
+        super().__init__(
+            key_value='conference.conf',
+            section='configuration',
+        )
+
+    def process(self, request):
+        """ Process conference configuration requests. """
+        template = 'conference/conference.conf.xml'
+        context = {
+            'domain': settings.PBX_HOSTNAME,
+        }
+        return template, context
+
+
+register_fsapi_handlers(
+    ConferenceConfigHandler(),
+)
